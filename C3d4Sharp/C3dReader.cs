cd5362c84e1947208896601e5f2792cc2bee31a8
lomelina@gmail.com <lomelina@gmail.com@ca529f79-5a5e-d70d-16fb-22ee8c3c1d4e>
2012-08-14 08:54:56 +0000

    Initial commit

---
+++ C3d4Sharp/C3dReader.cs
@@ -0,0 +1,355 @@
+﻿//-----------------------------------------------------------------------------
+// C3dReader.cs
+//
+// Reads in 3D position data from C3D file passed in "infile" 
+// (files should be opened before calling).
+//
+//
+// ETRO, Vrije Universiteit Brussel
+// Copyright (C) Lubos Omelina. All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
+
+namespace Vub.Etro.IO
+{
+    public class C3dReader
+    {
+        private string _c3dFile;
+        private FileStream _fs;
+        private BinaryReader _reader;
+        private Dictionary<string, ParameterGroup> _nameToGroups;
+        private Dictionary<int, ParameterGroup> _idToGroups;
+        private HashSet<Parameter> _allParameters;
+
+
+        private int _dataStart;
+        private int _pointsNumber;
+        private float _pointScale;
+        private float _pointRate;
+
+        private float _analogRate;
+        private int _analogUsed;
+        private float[] _analogScale;
+        private float _analogGenScale;
+        private Int16[] _analogZeroOffset;
+
+
+
+        #region Properties
+
+        private List<string> _pointsLabels;
+        private Dictionary<string,int> _pointsLabelsToId;
+        public IList<string> Labels { get { return _pointsLabels.AsReadOnly(); } }
+
+        private int _currentFrame = 0;
+        public int CurrentFrame { get { return _currentFrame; } }
+
+        private float[,] _analogData = null;
+        public float[,] AnalogData { get {return _analogData;} }
+
+        public Vector3[] _points = null;
+        public Vector3[] Points { get { return _points; } }
+
+        private int _pointFrames = 0;
+        public int FramesCount { get { return _pointFrames; } }
+
+        private C3dHeader _header = null;
+        public C3dHeader Header { get { return _header; } }
+
+        public bool IsInterger { get { return _pointScale >= 0; } }
+        public bool IsFloat { get { return _pointScale < 0; } }
+
+        #endregion
+
+        public C3dReader()
+        {
+            _nameToGroups = new Dictionary<string, ParameterGroup>();
+            _idToGroups = new Dictionary<int, ParameterGroup>();
+            _pointsLabels = new List<string>();
+            _pointsLabelsToId = new Dictionary<string, int>();
+            _allParameters = new HashSet<Parameter>();
+
+        }
+
+        public bool Open(string c3dFile)
+        {
+            _c3dFile = c3dFile;
+            try
+            {
+                _fs = new FileStream(_c3dFile, FileMode.Open, FileAccess.Read);
+                _reader = new BinaryReader(_fs);
+                _header = new C3dHeader();
+
+                ReadHeader();
+                ReadParameters();
+                ParseRequiredParameters();
+
+                _reader.BaseStream.Seek(_dataStart, 0);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("C3dReader.Open(\"" + c3dFile + "\"): " + e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private void ParseRequiredParameters()
+        {
+            string [] labels = GetParameter<string[]>("POINT:LABELS");
+            for (int i = 0; i < labels.Length; i++) 
+            {
+                string label = labels[i].TrimEnd(' ');
+                _pointsLabelsToId.Add(label, i);
+                _pointsLabels.Insert(i, label);
+            }
+            _dataStart = 512 * (GetParameter<Int16>("POINT:DATA_START") - 1);
+            _pointsNumber = GetParameter<Int16>("POINT:USED");
+            _pointFrames = GetParameter<Int16>("POINT:FRAMES");
+            _pointScale = GetParameter<float>("POINT:SCALE");
+            _pointRate = GetParameter<float>("POINT:RATE");
+
+            _analogRate = GetParameter<float>("ANALOG:RATE");
+            _analogUsed = GetParameter<Int16>("ANALOG:USED");
+            _analogScale = GetParameter<float[]>("ANALOG:SCALE");
+            _analogGenScale = GetParameter<float>("ANALOG:GEN_SCALE");
+            _analogZeroOffset = GetParameter<Int16[]>("ANALOG:OFFSET");
+        }
+
+
+
+        private void ReadHeader()
+        {
+            byte[] data = new byte[512];
+            int n = _reader.Read(data, 0, 512);
+            Header.SetHeader(data);
+
+            Console.WriteLine("\tFirstWord: {0:X}", Header.FirstWord);
+            Console.WriteLine("\tNumber of 3D points: " + Header.NumberOfPoints);
+            Console.WriteLine("\tAnalog channels: " + Header.AnalogChannels);
+
+            Console.WriteLine("\tFirst sample number: " + Header.FirstSampleNumber);
+            Console.WriteLine("\tLast sample number: " + Header.LastSampleNumber);
+            Console.WriteLine("\tScale factor: " + Header.ScaleFactor);
+            Console.WriteLine("\tData start: " + Header.DataStart);
+            Console.WriteLine("\tAnalog samples per frame: " + Header.AnalogSamplesPerFrame);
+            Console.WriteLine("\tFrame rate: " + Header.FrameRate);
+        }
+
+        private void ReadParameters()
+        {
+            byte[] parameters = new byte[4];
+            int nb = _reader.Read(parameters, 0, 4);
+            // TODO we should not ignore first 4 bytes as it is now
+
+            int nextItem;
+            do
+            {
+
+                sbyte nameLen = ParameterModel.ReadNameLength(_reader);
+                bool isLocked = nameLen < 0;
+                sbyte id = ParameterModel.ReadGroupID(_reader);
+                string name = ParameterModel.ReadName(_reader, Math.Abs(nameLen));
+                nextItem = ParameterModel.ReadNextItemOffset(_reader);
+
+                ParameterModel param = null;
+                int parameterDataSize = 0;
+                if (id > 0) //if id > 0 then it is parameter, otherwise it is group
+                {
+                    param = new Parameter(_reader);
+                    parameterDataSize = (param as Parameter).C3DParameterSize;
+                }
+                else
+                {
+                    param = new ParameterGroup();
+                }
+
+                byte descLen = ParameterModel.ReadDescLength(_reader);
+                string desc = ParameterModel.ReadName(_reader, descLen);
+
+                // general assignment
+                param.Name = name;
+                param.Id = id;
+                param.Description = desc;
+
+
+                if (param is Parameter)
+                {
+                    _allParameters.Add(param as Parameter);
+                }
+                else
+                {
+                    _nameToGroups.Add(param.Name, param as ParameterGroup);
+                    _idToGroups.Add(param.Id, param as ParameterGroup);
+                }
+
+                // if next item is more far read unknown data
+                // I assume that there will be no more than 512 bytes of unknown data
+
+                if (nextItem > (descLen + 3 + parameterDataSize)) // If the C3D file is correct this will never happen, but real life is far from perfect
+                {
+                    int toRead = nextItem - (descLen + 1);
+                    _reader.Read(parameters, 0, toRead);
+                }
+                //else if (nextItem < (descLen + 3 + parameterDataSize)) {
+                //    Console.Error.WriteLine("Error");
+                //}
+            } while (nextItem > 0);
+
+            foreach (Parameter p in _allParameters)
+            {
+                if (_idToGroups.ContainsKey(-p.Id))
+                {
+                    _idToGroups[-p.Id].Parameters.Add(p);
+                }
+            }
+        }
+
+        public T GetParameter<T>(string path)
+        {
+            string[] elements = path.Split(':');
+            if (elements.Length != 2)
+            {
+                throw new ApplicationException("Wrong path format (use GROUP:PARAMETER)");
+            }
+
+            if (!_nameToGroups.ContainsKey(elements[0]))
+                throw new ApplicationException("Group " + elements[0] + " deasn't exist.");
+
+            ParameterGroup grp = _nameToGroups[elements[0]];
+
+            foreach (Parameter p in grp.Parameters)
+            {
+                if (p.Name == elements[1])
+                    return p.GetData<T>();
+            }
+            throw new ApplicationException("Parameter " + elements[1] + " deasn't exist.");
+        }
+
+
+
+        public Vector3[] ReadFrame()
+        {
+            Vector3[] data;
+            if (_fs == null)
+            {
+                throw new ApplicationException("The C3d file is not open. Open the file before reading.");
+            }
+
+            if (_currentFrame >= _pointFrames)
+            {
+                _reader.BaseStream.Seek(_dataStart, 0);
+                _currentFrame = 0;
+            }
+
+            data = _pointScale < 0 ? ReadFloatData() : ReadIntData();
+
+            
+            
+            Console.WriteLine(_currentFrame);            
+            _currentFrame++;
+            return data;
+        }
+
+        private Vector3[] ReadFloatData()
+        {
+            if (!IsFloat)
+                throw new ApplicationException("Data stored in C3D file are in Inetger format. You are trying to read it as a Floating-point format.");
+
+            _points = new Vector3[_pointsNumber];
+            for (int i = 0; i < _pointsNumber; i++)
+            {
+                _points[i] = new Vector3(
+                    /* float x = */ _reader.ReadSingle(),
+                    /* float y = */ _reader.ReadSingle(),
+                    /* float z = */ _reader.ReadSingle()
+                );
+                int cc = (int)_reader.ReadSingle();
+            }
+
+
+            // Read Analog data
+            // TODO (works if there is no analog data)
+            return _points;
+        }
+
+        private Vector3[] ReadIntData()
+        {
+            if (!IsInterger)
+                throw new ApplicationException("Data stored in C3D file are in Floating-point format. You are trying to read it as a Integer format.");
+
+            _points = new Vector3[_pointsNumber];
+            for (int i = 0; i < _pointsNumber; i++)
+            {
+                _points[i] = new Vector3(
+                    /* float x = */ _reader.ReadInt16() * _pointScale,
+                    /* float y = */ _reader.ReadInt16() * _pointScale,
+                    /* float z = */ _reader.ReadInt16() * _pointScale
+                );
+                int cc = _reader.ReadInt16();
+            }
+
+            // reading of analog data
+            int samplesPerFrame = (int)(_analogRate / _pointRate);
+            _analogData = new float[samplesPerFrame, _analogUsed];
+            for (int rate = 0; rate < samplesPerFrame; rate++)
+            {
+                for (int channel = 0; channel < _analogUsed; channel++)
+                {
+                    float data = _reader.ReadInt16();
+                    _analogData[rate, channel] =
+                        (data - ((_analogZeroOffset != null && _analogZeroOffset.Length > 0) ? _analogZeroOffset[channel] : 0)) * _analogGenScale * (_analogScale != null && _analogScale.Length > 0 ? _analogScale[channel] : 1); 
+                }
+            }
+
+            return _points;
+        }
+
+        public Vector3 this[int key]
+        {
+            get
+            {
+                if (_points == null) { 
+                    throw new ApplicationException("You must open file and read freame first");
+                }
+                else if (key < 0 || key >= _points.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return _points[key];
+            }
+        }
+
+        public Vector3 this[string key]
+        {
+            get
+            {
+                if (_pointsLabels == null)
+                {
+                    throw new ApplicationException("You must open file and read freame first");
+                }
+                if (!_pointsLabelsToId.ContainsKey(key))
+                {
+                    throw new ApplicationException("Label " + key + " doesn't exist in the 3D point data section");
+                }
+                return _points[_pointsLabelsToId[key]];
+            }
+        }
+
+
+        public bool Close()
+        {
+            _reader.Close();
+            _fs.Close();
+            return true;
+        }
+
+    }
+}
+
